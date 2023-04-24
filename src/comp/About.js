c38259c4cdb2f9ba75ed3f0d63e85d0d5b1a3f34
dangabriel55 <cena_re_ra_dane@yahoo.com>
2023-04-24 11:32:18 +0300

    first commit

---
+++ src/comp/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+
+function About() {
+  return (
+    <div className='a'>
+        <div className='a-left'>left</div>
+        <div className='a-right'>right</div>
+    </div>
+    
+  )
+}
+
+export default About
